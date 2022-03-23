441969b3ce95aca8ca4dcfae64bc147be316e2ba
sangitasahu <sangeeta.vssut@gmail.com>
2022-03-22 21:04:22 -0400

    Commiting changes of global planner

--- src/global_planner/src/global_planner_node.py
+++ src/global_planner/src/global_planner_node.py
@@ -1,50 +1,178 @@
 #!/usr/bin/env python
 
+import math
 import rospy
 from map_simulator.msg import Map3D
-from geometry_msgs.msg import Vector3
-from snapstack_msgs.msg import Goal
-from snapstack_msgs.msg import State
-from trajectory_msgs.msg import JointTrajectoryPoint
+from snapstack_msgs.msg import Goal, State
+from geometry_msgs.msg import PoseStamped, Pose, Vector3
+from nav_msgs.msg import Path
+from sensor_msgs.msg import PointCloud, ChannelFloat32
 
+# Node : (x, y, z, occ_sts, cost)
+# PointCloud : (x, y, z), ChannelFloat32 : occupancy info
+# goal and state: (x, y, z)
+# modes: STRAIGHT or DIAGONAL
+# distance metrics: EUCLIDEAN or MANHATTAN
+# width of the Bounding Box : BB_WIDTH
+#
+#
+#
 
 class globalPlanner:
 	def __init__(self):
+		#Constants
+		self.DIAGONAL = 1
+		self.STRAIGHT = 2
+		self.BB_WIDTH = 5
+		self.EUCLIDEAN_DIST = 1
+		self.MANHATTAN_DIST = 0
+		self.INFINITE_COST = 1000
+		self.OCCUPIED = 1
+		self.NOT_OCCUPIED = 0
+		self.GRID_WIN_WIDTH = 3 #Keep it even
+		#Variables
+		self.move = self.STRAIGHT
 		self.num_of_cells = 0 
-		self.cur_goal = Goal()
-		self.cur_state = State()
-		self.map = Map3D()
-		self.global_traj = JointTrajectoryPoint()
-		self.global_traj_pub = rospy.Publisher('global_trajectory', JointTrajectoryPoint, queue_size=10)
+		self.cur_goal = Vector3()
+		self.cur_state = Vector3()
+		self.cur_node = []
+		self.new_nodes = []
+		self.map_points = PointCloud()
+		self.occupancy_sts = ChannelFloat32()
+		self.occupancy_sts.name = 'occupancy probability'
+		self.global_path = Path()
+		self.path_nodes_list = []
+
+		#Publishers
+		self.global_path_pub = rospy.Publisher('global_path', Path, queue_size=10)
+		#Subscribers		
+		self.goal_sub = rospy.Subscriber("/SQ01s/goal" , Goal, self.read_goal)
+		self.state_sub = rospy.Subscriber("/SQ01s/state" , State, self.read_state)
+		self.mapper_sub = rospy.Subscriber("probability_publisher" , PointCloud, self.read_map)
+		self.rviz_goal_sub = rospy.Subscriber("/move_base_simple/goal", PoseStamped, self.read_rviz_goal)
 	
-	def spin(self):
-	   	rospy.Subscriber("/SQ01s/goal" , Goal, self.read_goal)
-		rospy.Subscriber("/SQ01s/state" , State, self.read_state)
-		rospy.Subscriber("internal_map" , Map3D, self.read_map)
-		self.global_traj = self.generate_global_traj()
-		self.global_traj_pub.publish(self.global_traj)
-		rospy.spin()
-	
-	def read_map(self,msg):
-		self.map = msg	
-		rospy.loginfo(map.nodeCoordinates)
-		rospy.loginfo(map.occupiedStatus)
-		rospy.loginfo(map.nodeDistances)
-		rospy.loginfo(self.cur_goal.p)
-		rospy.loginfo(self.cur_state.pos)		
+	def read_rviz_goal(self, msg):	
+		self.cur_rviz_goal = [msg.pose.position.x,msg.pose.position.y,msg.pose.position.z]
+		rospy.loginfo(self.cur_rviz_goal)
 
 	def read_goal(self,msg):
-		self.cur_goal = msg;
+		self.cur_goal = msg.p;
+		self.dist_to_goal = self.calc_dist_btw_nodes(self.cur_state, self.cur_goal)
+		rospy.loginfo(self.cur_goal)
 
 	def read_state(self,msg):
-		self.cur_state = msg;
+		self.cur_state = msg.pos;
+		self.cur_node = [self.cur_state.x,self.cur_state.y, self.cur_state.z, 0, 0 ]
+		self.path_nodes_list.append(self.cur_node)
+		rospy.loginfo(self.cur_state)
+
+	def read_map(self,pointClouds):
+		self.map_points.points = pointClouds.points
+		self.occupancy_sts.values =pointClouds.channels[0].values	
+		rospy.loginfo(len(pointClouds))
+
+	def select_move(self,cur_node, node1, node2):
+		#node1 is straight and node2 is diagonal to the current node
+		if calc_cost_btw_nodes(cur_node, node1) < calc_cost_btw_nodes(cur_node,	node2):
+			return 	self.STRAIGHT
+		else:
+			return self.DIAGONAL		
+
+	def calc_dist_btw_nodes(self,node1, node2, typ):
+		if typ == self.MANHATTAN_DIST:
+			dist = math.abs(node2[0] -node1[0]) + math.abs(node2[1] - node1[1]) + math.abs(node2[2] - node1[2])
+		else:
+			dist = (math.abs(node2[0] -node1[0])**2) + (math.abs(node2[1] - node1[1])**2) + (math.abs(node2[2] - node1[2])**2)
+		return dist
 	
-	def generate_global_traj(self):		
-		self.global_traj.positions = [self.map.nodeCoordinates[i] for i in range(len(self.map.nodeCoordinates))]
-		self.global_traj.velocities = [0 for i in range(len(self.map.nodeCoordinates))]
-		self.global_traj.accelerations = [0 for i in range(len(self.map.nodeCoordinates))] 
-		self.global_traj.effort = [0 for i in range(len(self.map.nodeCoordinates))] 
-		self.global_traj.time_from_start = 0
+	def calc_cost_btw_nodes(self,node1, node2):
+		if node2[3] == 0: #Cost of empty cell = cur_node cost + dist between the two
+			return node1[4] + calc_dist_btw_nodes(node1, node2, self.MANHATTAN_DIST)
+		else: 	#Cost of occupied cell is infinity
+			return self.INFINITE_COST
+
+	def get_occupancy_sts(self,node):
+		node_idx = 0
+		occ_sts = SELF.OCCUPIED #For nodes not in the map, the status is OCCUPIED
+		for point in self.map_points.points:
+			if point == node[0:2]:
+				occ_sts = self.occupancy_sts.values[node_idx] 
+		return occ_sts
+	
+	def get_neighbours(self):
+		node_c = self.cur_node; 
+		d = math.floor((self.GRID_WIN_WIDTH - 1)/2)
+		#Select the next probable state
+		if self.move == self.STRAIGHT: #Straight Movements
+			if get_occupancy_sts(node_c + [1,0,0,0,0]) == self.NOT_OCCUPIED: # forward
+				node_s = node_c + [1,0,0,0,0]
+			elif get_occupancy_sts(node_c + [0,1,0,0,0]) == self.NOT_OCCUPIED: #left
+				node_s = node_c + [0,1,0,0,0]
+			elif get_occupancy_sts(node_c + [0,-1,0,0,0]) == self.NOT_OCCUPIED: #right
+				node_s = node_c + [0,-1,0,0,0]
+			elif get_occupancy_sts(node_c + [0,0,1,0,0]) == self.NOT_OCCUPIED:  #up
+				node_s = node_c + [0,0,1,0,0]
+			else:	#down
+				node_s = node_c + [0,0,-1,0,0]
+		else: #Diagonal Movements
+			if get_occupancy_sts(node_c + [1,1,0,0,0]) == self.NOT_OCCUPIED: # forward left
+				node_s = node_c + [1,1,0,0,0]
+			elif get_occupancy_sts(node_c + [1,-1,0,0,0]) == self.NOT_OCCUPIED: #forward right
+				node_s = node_c + [1,-1,0,0,0]
+			elif get_occupancy_sts(node_c + [1,1,1,0,0]) == self.NOT_OCCUPIED: #forward left up
+				node_s = node_c + [1,1,1,0,0]
+			elif get_occupancy_sts(node_c + [1,1,-1,0,0]) == self.NOT_OCCUPIED: #forward left down
+				node_s = node_c + [1,1,-1,0,0]
+			elif get_occupancy_sts(node_c + [1,-1,1,0,0]) == self.NOT_OCCUPIED: #forward right up
+				node_s = node_c + [1,-1,1,0,0]
+			elif get_occupancy_sts(node_c + [1,-1,-1,0,0]) == self.NOT_OCCUPIED: #forward right down
+				node_s = node_c + [1,-1,-1,0,0]
+			elif get_occupancy_sts(node_c + [1,0,1,0,0]) == self.NOT_OCCUPIED: #forward up
+				node_s = node_c + [1,0,1,0,0]
+			elif get_occupancy_sts(node_c + [1,0,-1,0,0]) == self.NOT_OCCUPIED: #forward down
+				node_s = node_c + [1,0,-1,0,0]
+			elif get_occupancy_sts(node_c + [0,1,1,0,0]) == self.NOT_OCCUPIED: #left up
+				node_s = node_c + [0,1,1,0,0]
+			elif get_occupancy_sts(node_c + [0,1,-1,0,0]) == self.NOT_OCCUPIED: #left down
+				node_s = node_c + [0,1,-1,0,0]
+			elif get_occupancy_sts(node_c + [0,-1,1,0,0]) == self.NOT_OCCUPIED: #right up
+				node_s = node_c + [0,-1,1,0,0]
+			else:  #right down
+				node_s = node_c + [0,-1,-1,0,0]
+		
+    		#Add the next probable state to the neighbours list and prune nodes around it		
+  		neighbours = node_s
+		for k in range(-d,d):
+			for j in range(-d,d):
+				for i in range(0,d):
+					node_i = node_c[0:2] + [i,j,k]
+					node_i.append(get_occupancy_sts(node_i))
+					if self.get_occupancy_sts(node_i) == self.NOT_OCCUPIED:
+						if self.calc_dist_btw_nodes(node_c , node_i) > (self.calc_dist_btw_nodes(node_c, node_s) + self.calc_dist_btw_nodes(node_s, node_i)): #Pruning Cells
+							neighbours.append(node_i.append(self.calc_dist_btw_nodes(node_s, node_i)))
+		
+		
+		node_d = self.cur_node + [GRID_WIN_WIDTH,GRID_WIN_WIDTH,0,0,0]
+		
+
+	def generate_global_path(self):
+		node_pose = Pose()
+		global_nodes_path = Path()
+		for path_node in self.path_nodes_list:			
+			node_pose.position.x = path_node[0];
+			node_pose.position.y = path_node[1];
+			node_pose.position.z = path_node[2];
+			node_pose.orientation.x = 0;
+			node_pose.orientation.y = 0;
+			node_pose.orientation.z = 0;
+			node_pose.orientation.w = 0;	
+			global_nodes_path.poses.append(node_pose);
+		return global_nodes_path		
+
+	def spin(self):		
+		self.global_path = self.generate_global_path()
+		self.global_path_pub.publish(self.global_path)
+		rospy.spin()
 
 if __name__ == '__main__':
 	rospy.init_node('globalPlanner')
--- src/mapper/octomap_listener/src/octomapListner.cpp
+++ src/mapper/octomap_listener/src/octomapListner.cpp
@@ -8,36 +8,65 @@
 #include <sensor_msgs/PointCloud.h>
 #include <vector>
 #include <tf/transform_listener.h>
-
-
+#include "snapstack_msgs/State.h"
+#include <string>
    
+
 ros::Publisher pubProb;
-//tf::StampedTransform transform;
+ros::Publisher pubPoints;
+tf::StampedTransform transform;
+snapstack_msgs::State quad_state;
+
+std::vector<std::vector<float>> sortedPointCloud(sensor_msgs::PointCloud& pointsCloud){
+    std::vector<geometry_msgs::Point32> points3d = pointsCloud.points;
+    std::vector<std::vector<float>> pointCloud;
+    std::vector<float> occupancyData = pointsCloud.channels[0].values;
+    int index = 0;
+    for(auto point:points3d){
+        pointCloud.push_back({point.x, point.y, point.z, occupancyData[index]});
+    }
+    std::sort(pointCloud.begin(), pointCloud.end());
+
+    return pointCloud;
+}
+
+void state_callback(const snapstack_msgs::State& state_msg)
+{
+    quad_state = state_msg;
+    geometry_msgs::Vector3 quad_pos = quad_state.pos;
+}
 
 void octomap_binary_callback(const octomap_msgs::OctomapConstPtr& octomap_msg)
 {
+
+    geometry_msgs::Vector3 quad_pos = quad_state.pos;
+    
     octomap::AbstractOcTree* tree = octomap_msgs::binaryMsgToMap(* octomap_msg);
 
     octomap::OcTree* octree = dynamic_cast<octomap::OcTree*>(tree);
 
     octomap::point3d  occupancyPoints3d; 
+    octomap::point3d  min_bbx;
+    min_bbx.x() = quad_pos.x-2;
+    min_bbx.y() = quad_pos.y-2;
+    min_bbx.z() = quad_pos.z-2;
+    octomap::point3d  max_bbx;
+    max_bbx.x() = quad_pos.x+5.;
+    max_bbx.y() = quad_pos.y+5.;
+    max_bbx.z() = quad_pos.z+5.;
+
     std::vector<_Float32> occupancyProb;
-    //tf::Vector3 vectorPoints3d;
+      
+    tf::Vector3 vectorPoints3d;
     geometry_msgs::Point32 point3d;
     sensor_msgs::ChannelFloat32 occupancyChannel;
     std::vector<geometry_msgs::Point32> points3d;
     std::vector<sensor_msgs::ChannelFloat32> occupancyChannels;
-    
-    for(octomap::OcTree::leaf_iterator it = octree->begin_leafs(), end = octree->end_leafs(); it!= end; ++it)
+
+
+    for(octomap::OcTree::leaf_bbx_iterator it = octree->begin_leafs_bbx(min_bbx,max_bbx), end = octree->end_leafs_bbx(); it!= end; ++it)
     {
         occupancyPoints3d = it.getCoordinate();
-        //vectorPoints3d = {occupancyPoints3d.x(),occupancyPoints3d.y(), occupancyPoints3d.z()}; 
-
-        //tf::Vector3 transformedPoints3d = transform.operator()(vectorPoints3d);
-
-        /*point3d.x = transformedPoints3d[0];
-        point3d.y = transformedPoints3d[1];
-        point3d.z = transformedPoints3d[2];*/
 
         point3d.x = occupancyPoints3d.x();
         point3d.y = occupancyPoints3d.y();
@@ -48,8 +77,7 @@ void octomap_binary_callback(const octomap_msgs::OctomapConstPtr& octomap_msg)
         occupancyProb.push_back(it->getOccupancy());
     }
     
-    
-    
+
     occupancyChannel.name = "occupancy probability";
     occupancyChannel.values = occupancyProb;
 
@@ -57,7 +85,7 @@ void octomap_binary_callback(const octomap_msgs::OctomapConstPtr& octomap_msg)
 
 
     sensor_msgs::PointCloud occupancy_info;
-    occupancy_info.header.frame_id = "SQ01s/camera";
+    occupancy_info.header.frame_id = "vicon";
     occupancy_info.header.stamp = ros::Time(0);
     occupancy_info.points = points3d;
     occupancy_info.channels = occupancyChannels;
@@ -70,23 +98,15 @@ void octomap_binary_callback(const octomap_msgs::OctomapConstPtr& octomap_msg)
 
 int main(int argc, char **argv)
 {
+
     ros::init(argc, argv, "octomapListener");
     ros::NodeHandle n;
     ros::Subscriber sub = n.subscribe("octomap_binary", 1000, octomap_binary_callback);
+    ros::NodeHandle state;
+    ros::Subscriber sub_state = state.subscribe("/SQ01s/state", 10, state_callback);
     ros::NodeHandle occupancyProbability; 
+    ros::NodeHandle points;
     pubProb = occupancyProbability.advertise<sensor_msgs::PointCloud>("probability_publisher",1000);
-    //tf::TransformListener listner;
-    /*
-    while (n.ok()){
-        try{
-            listner.waitForTransform("world","SQ01s/camera",ros::Time(0),ros::Duration(4));
-            listner.lookupTransform("world","SQ01s/camera",ros::Time(0),transform);
-        }
-        catch(tf::TransformException ex){
-            ROS_ERROR("%s", ex.what());
-        }
 
-    }
-    */
     ros::spin();    
 }
