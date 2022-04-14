72de7f41f1e6eea1b34512f036b39665cc1d4d2d
Furqan Ahmed <71396176+Furqan-Code@users.noreply.github.com>
2022-04-14 05:57:48 -0400

    Update octomapListner.cpp

--- src/mapper/src/octomapListner.cpp
+++ src/mapper/src/octomapListner.cpp
@@ -1,6 +1,7 @@
 #include "ros/ros.h"
 #include "octomap_msgs/Octomap.h"
 #include <octomap/AbstractOcTree.h>
+#include <octomap/OccupancyOcTreeBase.h>
 #include <octomap_msgs/Octomap.h>
 #include <octomap_msgs/conversions.h>
 #include <std_msgs/Float32MultiArray.h>
@@ -27,6 +28,7 @@ octomap::point3d  occupancyPoints3d;
 void state_callback(const snapstack_msgs::State& state_msg)
 {
     quad_state = state_msg;
+
 }
 
 
@@ -58,15 +60,14 @@ void octomap_binary_callback(const octomap_msgs::OctomapConstPtr& octomap_msg)
 {
 
     quad_pos = quad_state.pos;
-    
     //octomap::AbstractOcTree* tree = octomap_msgs::binaryMsgToMap(* octomap_msg);
 
     //octomap::OcTree* octree = dynamic_cast<octomap::OcTree*>(tree);
 
     std::shared_ptr<octomap::OcTree> octree = std::shared_ptr<octomap::OcTree> (dynamic_cast<octomap::OcTree*> (octomap_msgs::msgToMap(* octomap_msg)));
 
-    int z_scale_factor = 6;
-    float bbx_range = 18.;
+    int z_scale_factor = 3;
+    float bbx_range = 12.;
     float bbx_range_z = bbx_range/z_scale_factor;
     float resolution = 0.25;
     //int bsize = static_cast<int>((bbx_upper-bbx_lower)/resolution);
@@ -95,6 +96,71 @@ void octomap_binary_callback(const octomap_msgs::OctomapConstPtr& octomap_msg)
     std::vector<geometry_msgs::Point32> points3d;
     std::vector<sensor_msgs::ChannelFloat32> occupancyChannels;
 
+    // octomap::point3d const origin_coord(quad_pos.x, quad_pos.y, quad_pos.z);
+    // octomap::point3d const direction_coord(2., 3., 1.);
+    // octomap::point3d end_coord;
+
+    // bool occupied_ray = octree->castRay(origin_coord, direction_coord, end_coord, false, 0.);
+
+    // ROS_INFO("%f", end_coord.x());
+    // ROS_INFO("%f", end_coord.y());
+    // ROS_INFO("%f", end_coord.z());
+
+    // ROS_INFO("%i", occupied_ray);
+
+    // octomap::point3d const direction_coord1(8., 0., 1.375);
+    // octomap::point3d end_coord1;
+
+    // bool occupied_ray1 = octree->castRay(origin_coord, direction_coord1, end_coord1);
+
+    // ROS_INFO("%f", end_coord1.x());
+    // ROS_INFO("%f", end_coord1.y());
+    // ROS_INFO("%f", end_coord1.z());
+
+    // ROS_INFO("%i", occupied_ray1);
+
+    offset3d.x = quad_pos.x;
+    offset3d.y = quad_pos.y;
+    offset3d.z = quad_pos.z;
+    grids3d = grid_point_gen(offset3d,min_bbx,max_bbx,resolution);
+    gridOccupancy.resize(grids3d.size(), -1.);
+
+    for(int grid_index =0; grid_index<grids3d.size(); grid_index++){ 
+
+        float grid_x = grids3d[grid_index].x;
+        float grid_y = grids3d[grid_index].y;
+        float grid_z = grids3d[grid_index].z;
+
+        octomap::point3d const origin_coord(quad_pos.x, quad_pos.y, quad_pos.z);
+        octomap::point3d const direction_coord(grids3d[grid_index].x, grids3d[grid_index].y, grids3d[grid_index].z);
+        octomap::point3d end_coord;
+
+        bool occupied_ray = octree->castRay(origin_coord, direction_coord, end_coord);    
+
+        // ROS_INFO("%i",occupied_ray);
+        // ROS_INFO("g");
+
+        float norm_point = sqrt(pow(grid_x,2)+pow(grid_y,2)+pow(grid_z,2));
+        float norm_node = sqrt(pow(end_coord.x(),2)+pow(end_coord.y(),2)+pow(end_coord.z(),2));
+
+        if(occupied_ray){
+
+            if(norm_node-norm_point>0.01){
+                gridOccupancy[grid_index]=0.;
+            }
+            //else if(abs(norm_node-norm_point)<resolution){
+            //    gridOccupancy[grid_index]=1.;
+            //}
+        }
+        else{
+            if(norm_node-norm_point>0) {
+                gridOccupancy[grid_index]=0.;
+            }
+            
+        }  
+
+    }
+
     int tree_index = 0;
     for(octomap::OcTree::leaf_bbx_iterator it = octree->begin_leafs_bbx(min_bbx,max_bbx), end = octree->end_leafs_bbx(); it!= end; ++it)
     {
@@ -111,27 +177,35 @@ void octomap_binary_callback(const octomap_msgs::OctomapConstPtr& octomap_msg)
         point3d.z = round_val(point3d.z);
 
         //box limits
+        float frontLim;
+        float rearLim;
+        float leftLim;
+        float rightLim;
+        float upperLim;
+        float lowerLim;
 
-        float frontLim = point3d.x + resolution;
-        float rearLim = point3d.x - resolution;
-        float leftLim = point3d.y + resolution;
-        float rightLim = point3d.y - resolution;
-        float upperLim = point3d.z + resolution;
-        float lowerLim = point3d.z - resolution;
-
-
-
-        if(tree_index==1){
-            offset3d.x = quad_pos.x;
-            offset3d.y = quad_pos.y;
-            offset3d.z = quad_pos.z;
-            grids3d = grid_point_gen(offset3d,min_bbx,max_bbx,resolution);
-            gridOccupancy.resize(grids3d.size(), -1.);
+        if(it->getOccupancy()>0.5){
+            frontLim = point3d.x + (resolution+0.1*resolution);
+            rearLim = point3d.x - (resolution+0.1*resolution);
+            leftLim = point3d.y + (resolution+0.1*resolution);
+            rightLim = point3d.y - (resolution+0.1*resolution);
+            upperLim = point3d.z + (resolution+0.1*resolution);
+            lowerLim = point3d.z - (resolution+0.1*resolution);
+        }
+        else{
+            frontLim = point3d.x + (resolution-0.2*resolution);
+            rearLim = point3d.x - (resolution-0.2*resolution);
+            leftLim = point3d.y + (resolution-0.2*resolution);
+            rightLim = point3d.y - (resolution-0.2*resolution);
+            upperLim = point3d.z + (resolution-0.2*resolution);
+            lowerLim = point3d.z - (resolution-0.2*resolution);
         }
 
-        //ROS_INFO("grid");
-        for(int grid_index =0; grid_index<grids3d.size(); grid_index++){   
-                        
+
+
+        for(int grid_index =0; grid_index<grids3d.size(); grid_index++){ 
+
+
             //if(abs(grids3d[grid_index].x-point3d.x)<1e-3 && abs(grids3d[grid_index].y-point3d.y)<1e-3 && abs(grids3d[grid_index].z-point3d.z)<1e-3){
             if (grids3d[grid_index].x>=rearLim && grids3d[grid_index].x<=frontLim && grids3d[grid_index].y>=rightLim && grids3d[grid_index].y<=leftLim && grids3d[grid_index].z>=lowerLim && grids3d[grid_index].z<=upperLim){
                 if(it->getOccupancy()>0.5){
@@ -150,6 +224,7 @@ void octomap_binary_callback(const octomap_msgs::OctomapConstPtr& octomap_msg)
 
     }
     
+    //ROS_INFO("%d", gridOccupancy.size());
 
     occupancyChannel.name = "occupancy probability";
     occupancyChannel.values = occupancyProb;
